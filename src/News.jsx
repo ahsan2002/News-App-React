4df4eae2a7291fcfb38d6bf09d12fb63d2402dde
Ahsan <omerjee.ahsan@gmail.com>
2022-10-21 19:22:04 +0500

    completed

--- src/News.jsx
+++ src/News.jsx
@@ -1,47 +1,110 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 const News = () => {
     const [data, setdata] = useState([]);
+    const [query, setQuery] = useState();
 
-    const getnewsapi = async () => {
-        const res = await fetch(
-            `https://newsapi.org/v2/everything?q=apple&from=2022-10-18&to=2022-10-18&sortBy=popularity&apiKey=4ce05c0b050a400ca73d55a48ff224cb`); //in pending state
-        const actualdata = await res.json();
-        // console.log(actualdata.articles);
-        setdata(actualdata.articles);
-    };
+    const getNews = (e) => {
+        e.preventDefault();
 
-    
+
+        const options = {
+            method: 'GET',
+            url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+            params: { q: query, freshness: 'Day', textFormat: 'Raw', safeSearch: 'Off' },
+            headers: {
+                'X-BingApis-SDK': 'true',
+                'X-RapidAPI-Key': '85KnnuP4HzmshYuCcfjg1sCMFdYkp18e8NojsnQP6hFvDHXrBr',
+                'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+            }
+        };
+
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data.value);
+                setdata(response.data.value)
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }
+
+
+    function getTrendingNews() {
+
+        const options = {
+            method: 'GET',
+            url: 'https://bing-news-search1.p.rapidapi.com/news',
+            params: { safeSearch: 'Off', textFormat: 'Raw' },
+            headers: {
+                'X-BingApis-SDK': 'true',
+                'X-RapidAPI-Key': '85KnnuP4HzmshYuCcfjg1sCMFdYkp18e8NojsnQP6hFvDHXrBr',
+                'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options)
+            .then(function (response) {
+                console.log(response.data);
+
+                setdata(response.data.value)
+
+            }).catch(function (error) {
+                console.error(error);
+            });
+    }
+
+
+
     useEffect(() => {
-        getnewsapi();
+
+        getTrendingNews();
+
     }, []);
 
 
     return (
         <>
+            <div className="center">
+                <form onSubmit={getNews}>
+                    <input
+                        type="text"
+                        placeholder="Enter your topic name"
+                        onChange={(e) => {
+                            setQuery(e.target.value)
+                        }}
+                    />
+                    <button className='mybtn' type="submit">Get News</button>
+                </form>
+            </div>
             {
                 data.map((elem) => {
-                    const date = new Date(elem.publishedAt);
+                    const date = new Date(elem?.datePublished);
                     const mydate = date.toLocaleDateString();
                     return (
-                        <div className="main">
+                        <div className="main" key={elem?.name}>
                             <div className="main1">
 
                                 <div className="content">
-                                    <h2>{elem.title}</h2>
-                                    <p>{elem.description}</p>
+                                    <a href={elem?.url} target='_blank' rel="noreferrer">
+
+                                        <h2>{elem?.name}</h2>
+                                    </a>
+                                    <p>{elem?.description}</p>
+
+
 
                                     <div className="date-time">
                                         <p>{mydate}</p>
-                                        <p>{elem.source.name}</p>
-                                        <a href={elem.url} target='_blank'> <button>Read More</button></a>
+                                        <p>{elem?.provider[0]?.name}</p>
                                     </div>
-
-
                                 </div>
 
                                 <div className="myimage">
-                                    <img style={{ height: "150px", width: "150px" }} src={elem.urlToImage} alt="img" />
+                                    <img style={{ height: "150px", width: "150px" }} src={elem?.image?.thumbnail?.contentUrl} alt="img" />
                                 </div>
 
 
